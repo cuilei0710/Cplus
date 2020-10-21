f1c7f36ed088a9c0a4977fd39278c9bee6e64775
崔小葵 <72482102+cuilei0710@users.noreply.github.com>
2020-10-21 15:24:04 +0800

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+int main()
+{
+    int n_int = INT_MAX;
+    cout << "int is " << sizeof(int) << " bytes." << endl;
+    cout << "int is " << sizeof n_int << " bytes." << endl;
+    system("pause");
+    return 0;
+}
+//Author: 崔小葵
+//Date: 2020-10-14
