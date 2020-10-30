da0bf7d07c618ce71092704a5065bbc0e2861831
cuilei0710 <cuilei0710@icloud.com>
2020-10-30 19:46:48 +0800

    enter

---
+++ C++ 4 chapter/delete.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstring> // or string.h
+
+using namespace std;
+
+char * getname(void); // function prototype
+int main(){
+
+}
+char * getname() // return pointer to new string
+{
+    char temp[80];
+}
