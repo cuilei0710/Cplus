fdf5599171aea1c213ae8c87997134861d8d3e88
cuilei0710 <cuilei0710@icloud.com>
2020-10-30 20:03:46 +0800

    enter

--- C++ 4 chapter/delete.cpp
+++ C++ 4 chapter/delete.cpp
@@ -3,11 +3,30 @@
 
 using namespace std;
 
-char * getname(void); // function prototype
-int main(){
-
-}
-char * getname() // return pointer to new string
+char *getname(void); // function prototype
+int main()
 {
-    char temp[80];
+    char *name; // create pointer but no storage
+
+    name = getname(); // assign address of string to name
+    cout << name << " at " << (int *)name << "\n";
+    delete[] name; // memory freed
+
+    name = getname(); // reuse freed memory
+    cout << name << " at " << (int *)name << "\n";
+    delete[] name; // memory freed again
+    system("pause");
+    return 0;
 }
+char *getname() // return pointer to new string
+{
+    char temp[80]; // temporary storage
+    cout << "Enter last name: ";
+    cin >> temp;
+    char *pn = new char[strlen(temp) + 1];
+    strcpy(pn, temp); // copy string into smaller space
+
+    return pn; // temp lost when function ends
+}
+// Author: 崔小葵
+// Date: 2020-10-30
