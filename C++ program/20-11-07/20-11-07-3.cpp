f105f6090b1457649777251c12d1158cbab6fdf5
cuilei0710 <cuilei0710@icloud.com>
2020-11-07 20:18:29 +0800

    enter

--- C++ program/20-11-07/20-11-07-3.cpp
+++ C++ program/20-11-07/20-11-07-3.cpp
@@ -2,7 +2,16 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
+    int i = 0, sum = 0;
+    while (i <= 100)
+    {
+        sum += i;
+        i++;
+    }
+    cout << "SUM = " << sum << endl; /*  */
+
     system("pause");
     return 0;
 }
