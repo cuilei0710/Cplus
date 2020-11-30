7d5fdfae40529a413537039db774ead8b7e92545
your name <CuiLei@567902>
2020-11-30 19:57:30 +0800

    leilei

---
+++ C++ 8 chapter/leftover.cpp
@@ -0,0 +1,16 @@
+/*
+ * @Author: 崔小葵
+ * @Date: 2020-11-30 19:45:24
+ * @Last Modified by: 崔小葵
+ * @Last Modified time: 2020-11-30 19:49:57
+ */
+#include <iostream>
+
+unsigned long left(unsigned long num, unsigned ct);
+char * left(const char * str, int n = 1);
+
+int main(){
+    using namespace std;
+    char * trip = "Hawaii!!"; // test value
+
+}
