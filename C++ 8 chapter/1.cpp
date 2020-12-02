3fc442e3fb3f40f2206876af134677b2ecd30062
cuilei0710 <cuilei0710@icloud.com>
2020-12-02 20:25:16 +0800

    leilei

---
+++ C++ 8 chapter/1.cpp
@@ -0,0 +1,9 @@
+/*
+ * @Author: mikey.zhaopeng
+ * @Date: 2020-12-02 20:23:55
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2020-12-02 20:24:27
+ */
+#include <iostream>
+
+using namepace std;
