3caed1b541ba9b8ca039840f97f8b820abebef79
cuilei0710 <cuilei0710@icloud.com>
2020-12-07 23:22:23 +0800

    leilei

--- C++ 8 practice/8.8.4.cpp
+++ C++ 8 practice/8.8.4.cpp
@@ -8,30 +8,54 @@
 #include <cstring> // for strlen(), strcpy()
 
 using namespace std;
-
 struct stringy
 {
     char *str; // pointer to string
     int ct;    // length of string (not counting '\0')
 };
 
+void set(struct stringy &in_stringy, char *in_string)
+{
+    in_stringy.ct = strlen(in_string);
+    in_stringy.str = new char[(in_stringy.ct) + 1];
+    strcpy(in_stringy.str, in_string);
+}
+
+void show(const struct stringy &in_stringy, int print_times = 1)
+{
+    for (size_t i = 0; i < print_times; i++)
+    {
+        cout << "Member string of struct stringy: " << in_stringy.str << endl;
+    }
+}
+
+void show(const char *str, int print_times = 1)
+{
+    for (size_t i = 0; i < print_times; i++)
+    {
+        cout << "Print char string: " << str << endl;
+    }
+}
+
 // prototypes for set(), show(), and show() go here
 int main()
 {
-    string benny;
-    char testing[] = "Reality isn't what is used to be."
+    string bea_ny;
+    char testing[] = "Reality isn't what is used to be.";
 
-        set(benny, testing); // first argument is a reference
+    set(bea_ny, testing);
+    // first argument is a reference
     // allocates space to hold copy of testing,
     // set str member of benny to point to the
     // new block, copies testing to new block
     // and sets ct member of benny
-    show(benny);    // prints member string once
-    show(benny, 2); // prints member string twice
+    show(bea_ny);    // prints member string once
+    show(bea_ny, 2); // prints member string twice
     testing[0] = 'D';
     testing[1] = 'u';
     show(testing);    // prints testing string once
     show(testing, 3); // prints testing string thrice
     show("Done!");
+    system("pause");
     return 0;
 }
