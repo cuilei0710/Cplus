b3b127439d2c1db9115957c4f66ebc8f01d0f183
cuilei0710 <cuilei0710@icloud.com>
2020-12-11 20:43:30 +0800

    leilei

---
+++ C++ 9 practice/9.6.2.cpp
@@ -0,0 +1,8 @@
+//Author: 崔小葵
+//Date: 2020-12-11 20:36:50
+//Last Modified by:   崔小葵
+//Last Modified time: 2020-12-11 20:36:50
+#include <iostream>
+#include <cstring>
+
+void strcount(const *str);
