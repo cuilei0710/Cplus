41c76f1cc441e2fbee1837803bf7dc1c3d73f5e9
cuilei0710 <cuilei0710@icloud.com>
2020-12-11 23:00:43 +0800

    leilei

---
+++ C++ Primer Plus/C++ 9 practice/9.6.4/9.6.4.h
@@ -0,0 +1,26 @@
+//Author: 崔小葵
+//Date: 2020-12-11 22:49:54
+//Last Modified by: 崔小葵
+//Last Modified time: 2020-12-11 22:49:54
+namespace SALES
+{
+    const int QUARTERS = 4;
+    struct Sales
+    {
+        double sales[QUARTERS];
+        double average;
+        double max;
+        double min;
+    };
+    // copies the lesser of 4 or n items from the array ar
+    // to the sales member of s ang computes and stores the
+    // average, maximum values of the entered items;
+    // remaining elements of sales, if any, set to 0
+    void setSales(Sales &s, const double ar[], int n);
+    // gathers sales for 4 quarters interactively, stores them
+    // in the sales member of s and computes and stores the
+    // average, maximum, and minmum values
+    void setSales(Sales &s);
+    // display all information in structure s
+    void showSales(const Sales &s);
+} // namespace name
