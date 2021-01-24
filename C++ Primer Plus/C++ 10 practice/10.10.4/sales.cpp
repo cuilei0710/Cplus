f0f64ca525b2df4c78118be3f2d52dd50bcca552
cuilei0710 <cuilei0710@icloud.com>
2021-01-24 13:31:18 +0800

    leilei

--- C++ Primer Plus/C++ 10 practice/10.10.4/sales.cpp
+++ C++ Primer Plus/C++ 10 practice/10.10.4/sales.cpp
@@ -6,30 +6,40 @@
 
 using namespace std;
 using namespace SALES;
-
-SALES::Sales(){
+Sales::Sales(){
     double total = 0.0;
     double max = 0.0;
     double min = 0.0;
     double input = 0.0;
+
     cout << "Enter 4 double number: " << endl;
-    for (size_t i = 0; i < QUARTERS; i++)
-    {
+    for (size_t i = 0; i < QUARTERS; i++){
         cin >> input;
-        if(i == 0){
-            max = input;
-            min = input;
-        }
-        if (max > input)
+        if (i == 0)
         {
-            max = input;
+            max = min = input;
         }
-        if (min < input)
+        if (min > input)
         {
             min = input;
+        }
+        if(max < input){
+            max = input;
         }
         total += input;
         sales[i] = input;
     }
-    this
+    this->average = total / QUARTERS;
+    this->max = max;
+    this->min = min;
+}
+
+Sales::Sales(double ar[], int n){
+    double total = 0.0;
+    double max = ar[0];
+    double min = ar[0];
+    for (size_t i = 0; i < n; i++){
+        total += ar[i];
+        if
+    }
 }
